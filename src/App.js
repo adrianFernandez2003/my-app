8609333c16e20ace1776adb31c8d0aa1bae8c89a
adrian <sun-moon1814@hotmail.com>
2024-04-16 16:05:19 -0600

    prueba

--- src/App.js
+++ src/App.js
@@ -4,20 +4,65 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+      <nav>
+        <a href="user.html">usuarios</a>
+        <a href="products.html">productos</a>
+        <a href="productSession.html">sesion de productos</a>
+        <a href="session.html">sesiones</a>
+        <a href="gender.html">genero</a>
+        <a href="adress.html">direccion</a>
+        <a href="customer.html">clientes</a>
+        <a href="categories.html">categorias</a>
+    </nav>
       </header>
+      <main className="App-main">
+        <div className='card'>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+          <p>Sed ullamcorper dolor at velit tempor, ac bibendum sem interdum. Pellentesque fermentum ornare eros ac facilisis.</p>
+          <button>ver mas</button>
+        </div>
+        <div className='card'>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+          <p>Sed ullamcorper dolor at velit tempor, ac bibendum sem interdum. Pellentesque fermentum ornare eros ac facilisis.</p>
+          <button>ver mas</button>
+        </div>
+        <div className='card'>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+          <p>Sed ullamcorper dolor at velit tempor, ac bibendum sem interdum. Pellentesque fermentum ornare eros ac facilisis.</p>
+          <button>ver mas</button>
+        </div>
+        <div className='card'>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+          <p>Sed ullamcorper dolor at velit tempor, ac bibendum sem interdum. Pellentesque fermentum ornare eros ac facilisis.</p>
+          <button>ver mas</button>
+        </div>
+        <div className='card'>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+          <p>Sed ullamcorper dolor at velit tempor, ac bibendum sem interdum. Pellentesque fermentum ornare eros ac facilisis.</p>
+          <button>ver mas</button>
+        </div>
+        <div className='card'>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+          <p>Sed ullamcorper dolor at velit tempor, ac bibendum sem interdum. Pellentesque fermentum ornare eros ac facilisis.</p>
+          <button>ver mas</button>
+        </div>
+        <div className='card'>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+          <p>Sed ullamcorper dolor at velit tempor, ac bibendum sem interdum. Pellentesque fermentum ornare eros ac facilisis.</p>
+          <button>ver mas</button>
+        </div>
+        <div className='card'>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+          <p>Sed ullamcorper dolor at velit tempor, ac bibendum sem interdum. Pellentesque fermentum ornare eros ac facilisis.</p>
+          <button>ver mas</button>
+        </div>
+        <div className='card'>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+          <p>Sed ullamcorper dolor at velit tempor, ac bibendum sem interdum. Pellentesque fermentum ornare eros ac facilisis.</p>
+          <button>ver mas</button>
+        </div>
+      </main>
     </div>
   );
 }
