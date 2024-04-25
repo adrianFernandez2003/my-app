f93cae54ba8c64ffd4662ba2769fa5af18200aef
adrian <sun-moon1814@hotmail.com>
2024-04-24 20:18:37 -0600

    act12

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://gpkqqlpvetttdamjgexd.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwa3FxbHB2ZXR0dGRhbWpnZXhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzMTE0MzksImV4cCI6MjAyODg4NzQzOX0.e8LUyRZKaWKDwqbV4dRVgcBuarggCDI3sd5J1vuWyUQ");
+
 
 function App() {
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    getUsuarios();
+  }, []);
+
+  async function getUsuarios() {
+    const { data } = await supabase.from("usuarios").select();
+    setUsuarios (data);
+  }
+
   return (
     <div className="App">
       <header className='App-header'>
@@ -17,6 +33,11 @@ function App() {
     </nav>
       </header>
       <main className="App-main">
+      <ul>
+        {usuarios.map((usuario) => (
+          <li key={usuario.nombre}>{usuario.nombre}</li>
+        ))}
+      </ul>
         <div className='card'>
           <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
           <p>Sed ullamcorper dolor at velit tempor, ac bibendum sem interdum. Pellentesque fermentum ornare eros ac facilisis.</p>
@@ -64,6 +85,7 @@ function App() {
         </div>
       </main>
     </div>
+    
   );
 }
 
